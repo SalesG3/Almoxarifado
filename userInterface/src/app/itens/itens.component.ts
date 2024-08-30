5cfab96be0e5633f8e46ad2592aadbdfcb488846
Sales <contabilgs3@gmail.com>
2024-08-30 17:24:48 -0300

    show & hidden toggle on cadastrar

--- userInterface/src/app/itens/itens.component.ts
+++ userInterface/src/app/itens/itens.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-itens',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './itens.component.html',
   styleUrl: './itens.component.css'
 })
-export class ItensComponent {
+export class ItensComponent implements OnInit {
+
   registerStatus : string = "Consultando";
+
+  ngOnInit(): void {
+    
+  }
+
+  cadastrar(){
+
+    document.querySelector('.CRUD')?.classList.toggle('hidden');
+    document.querySelector('.detailMode')?.classList.toggle('showNav');
+
+    document.querySelector('.detalhesItem')?.classList.toggle('show');
+    document.querySelector('.bodyMain')?.classList.toggle('hidden');
+
+  }
 }
