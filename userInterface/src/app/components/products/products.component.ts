2f7249994593cce4399263c5f4d8f0e51fe9afbb
Sales <contabilgs3@gmail.com>
2024-09-13 08:13:48 -0300

    update for pull

--- userInterface/src/app/components/products/products.component.ts
+++ userInterface/src/app/components/products/products.component.ts
@@ -7,23 +7,17 @@ import { AfterViewInit, Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent implements AfterViewInit {
+export class ProductsComponent {
 
-  inner : any;
+  async lookup (tabela : string) {
 
-  constructor ( ) {  }
-
-  ngAfterViewInit(): void {
-    this.lookup()
-  }
-
-  async lookup ( ) {
-
-    let req = await fetch('http://localhost:8000/categorias/')
+    let req = await fetch(`http://localhost:8000/${tabela}/`)
     .then(req => req.json()).then((data) => {
+      (document.querySelector(`#${tabela}`) as HTMLElement).innerHTML = '<option hidden></option>';
 
       for(let i in data){
-        (document.querySelector('#categoria') as HTMLElement).innerHTML += `<option>${data[i].nome}</option>`
+        (document.querySelector(`#${tabela}`) as HTMLElement).innerHTML += 
+        `<option value="${data[i].id}">${data[i].nome}</option>`
       }
     })
   }
